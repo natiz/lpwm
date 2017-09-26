51c1f87671ee8b771e923fc2f1cf34bcfcfd9724
vx-lp <shaim7777@gmail.com>
2017-09-26 14:46:08 +0300

    login function and reuse
    Separate the ajax login into a function, and reuse when we have a username but no access_token.

--- script.js
+++ script.js
@@ -15,48 +15,55 @@ if (site && username && access_token) {
         callback(access_token, window.location.href);
     };
 }
-else {
+else if (site && username) {
+    ajaxLogin(site, username);
+} else {
     $("#lp_form").submit(function (e) {
         e.preventDefault();
         var url = "https://exampleauth.cloud.lprnd.net:1980";
         var site = $("#lp_account").val();
         var username = $("#lp_username").val();
-        var href;
+	ajaxLogin(site, username);
+    });
+}
 
-        $.ajax({
-            url: 'https://barakauth.auth0.com/oauth/token',
-            contentType: 'application/json',
-			type: 'POST',
-			processData: false,
-            data: JSON.stringify({
-                client_id: 'BscG0X3K6R8dgFZf90ydTUOETQztDvrq',
-	    		client_secret: 'u1qHTi_DQ7GVy-Z_9jI7wknEppVpvKYFZ7nTf_JrXlaRDVRorEz3xni5mG_4v7k0',
-	    		audience: 'https://barakauth.auth0.com/api/v2/',
-	    		grant_type: 'client_credentials'
-            }),
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                console.error('Failed to generate JWT', textStatus, errorThrown);
-            },
-            success: function (data) {
-                href = updateQueryStringParameter(window.location.href, "site", site);
-                href = updateQueryStringParameter(href, "username", username);
-                href = updateQueryStringParameter(href, "access_token", (data && data.access_token));
-                window.location.href = href;
-            }
-        });
+function ajaxLogin(site, username) {
+    var href;
 
-        function updateQueryStringParameter(uri, key, value) {
-            var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-            var separator = uri.indexOf('?') !== -1 ? "&" : "?";
-            if (uri.match(re)) {
-                return uri.replace(re, '$1' + key + "=" + value + '$2');
-            }
-            else {
-                return uri + separator + key + "=" + value;
-            }
+    $.ajax({
+        url: 'https://barakauth.auth0.com/oauth/token',
+        contentType: 'application/json',
+        type: 'POST',
+        processData: false,
+        data: JSON.stringify({
+            client_id: 'BscG0X3K6R8dgFZf90ydTUOETQztDvrq',
+            client_secret: 'u1qHTi_DQ7GVy-Z_9jI7wknEppVpvKYFZ7nTf_JrXlaRDVRorEz3xni5mG_4v7k0',
+            audience: 'https://barakauth.auth0.com/api/v2/',
+            grant_type: 'client_credentials'
+        }),
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.error('Failed to generate JWT', textStatus, errorThrown);
+        },
+        success: function (data) {
+            href = updateQueryStringParameter(window.location.href, "site", site);
+            href = updateQueryStringParameter(href, "username", username);
+            href = updateQueryStringParameter(href, "access_token", (data && data.access_token));
+            window.location.href = href;
         }
     });
 }
+
+function updateQueryStringParameter(uri, key, value) {
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+	return uri.replace(re, '$1' + key + "=" + value + '$2');
+    }
+    else {
+	return uri + separator + key + "=" + value;
+    }
+}
+
 
 $('#lp_lnk_setup').click(function () {
     var isDescriptionDisplay = $('#lp_account_setup_description').css('display') === 'block';
